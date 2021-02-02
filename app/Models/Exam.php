b7356ad61eb1ae3f8350a82ae1b9f7a28c483b7c
ahmed-hassan-sadek <77862913+ahmed-hassan-sadek@users.noreply.github.com>
2021-02-03 00:02:06 +0200

    2021_02_3

---
+++ app/Models/Exam.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Exam extends Model
+{
+    use HasFactory;
+
+    public function skill()
+    {
+        return $this->belongsTo(Skill::class);
+    }
+
+    public function Questions()
+    {
+        return $this->hasMany(Question::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
---
+++ database/migrations/2021_02_02_213550_create_settings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('email' , 255);
+            $table->string('phone' , 30);
+            $table->string('facebook' , 255)->mullable();
+            $table->string('teittwer' , 255)->mullable();
+            $table->string('instagram' , 255)->mullable();
+            $table->string('youtube' , 255)->mullable();
+            $table->string('likedin' , 255)->mullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
